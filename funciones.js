aa07af508a43c9213b0adba30b2dc00c11339837
Alexis Romero <romero.carlos6357@gmail.com>
2023-10-06 15:48:08 -0500

    se agregan las vistas iniciales con estilizacion basica, definiendo un header general

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function mostrarAlerta() {
+    alert("Armemos una pinta bien mela!");
+}
+
+function mostrarOpciones() {
+    document.querySelector('.lista-opciones').classList.toggle('visible');
+}
